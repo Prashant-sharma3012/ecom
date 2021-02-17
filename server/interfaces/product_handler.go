5f49b4aea4a485f0d5a45523794370acc00e94b5
prashant sharma <prashant.sharma@enquero.com>
2021-02-17 16:10:11 +0530

    added routes

---
+++ server/interfaces/product_handler.go
@@ -0,0 +1,112 @@
+package interfaces
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Prashant-sharma3012/ecom/tree/main/server/application"
+	"github.com/Prashant-sharma3012/ecom/tree/main/server/domain/entity"
+	"github.com/gorilla/mux"
+)
+
+var productAPIInstance *productAPI
+
+type productAPI struct {
+	pa *application.ProductApp
+}
+
+func SetupProductRoutes(r *mux.Router, p *application.ProductApp) {
+	productAPIInstance = &productAPI{p}
+
+	r.HandleFunc("/product/all", productAPIInstance.getProducts).Methods("GET")
+	r.HandleFunc("/product", productAPIInstance.createProduct).Methods("POST")
+	r.HandleFunc("/products/:productId", productAPIInstance.getProductByID).Methods("GET")
+	r.HandleFunc("/products/:productId", productAPIInstance.updateProduct).Methods("PUT")
+	r.HandleFunc("/product/:productId", productAPIInstance.deleteProduct).Methods("DELETE")
+}
+
+func (p *productAPI) getProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := p.pa.GetAllProduct(0, 10)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	response, err := json.Marshal(products)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (p *productAPI) createProduct(w http.ResponseWriter, r *http.Request) {
+	var product *entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	savedProduct, err := p.pa.SaveProduct(product)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	response, err := json.Marshal(savedProduct)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (p *productAPI) getProductByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productId := vars["productId"]
+
+	product, err := p.pa.GetProduct(productId)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	response, err := json.Marshal(product)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (p *productAPI) updateProduct(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	// productId := vars["productId"]
+
+	var product *entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	message, err := p.pa.UpdateProduct(product)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(message)
+}
+
+func (p *productAPI) deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productId := vars["productId"]
+
+	message, err := p.pa.DeleteProduct(productId)
+	if err != nil {
+		w.Write([]byte("Error Occured"))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(message)
+}
