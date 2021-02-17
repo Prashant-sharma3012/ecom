7df30404098630aac4acff721443971a81a6a4bf
prashant sharma <prashant.sharma@enquero.com>
2021-02-17 18:13:24 +0530

    added server logic for listening to api calls

--- server/interfaces/product_handler.go
+++ server/interfaces/product_handler.go
@@ -2,6 +2,8 @@ package interfaces
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/Prashant-sharma3012/ecom/tree/main/server/application"
@@ -9,14 +11,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var productAPIInstance *productAPI
+var productAPIInstance productAPI
 
 type productAPI struct {
-	pa *application.ProductApp
+	pa application.ProductAppInterface
 }
 
-func SetupProductRoutes(r *mux.Router, p *application.ProductApp) {
-	productAPIInstance = &productAPI{p}
+func SetupProductRoutes(r *mux.Router, p application.ProductAppInterface) {
+	productAPIInstance = productAPI{
+		pa: p,
+	}
 
 	r.HandleFunc("/product/all", productAPIInstance.getProducts).Methods("GET")
 	r.HandleFunc("/product", productAPIInstance.createProduct).Methods("POST")
@@ -26,16 +30,23 @@ func SetupProductRoutes(r *mux.Router, p *application.ProductApp) {
 }
 
 func (p *productAPI) getProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("getting Products")
 	products, err := p.pa.GetAllProduct(0, 10)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
+	log.Println("completed")
+
+	log.Println("creating response")
 	response, err := json.Marshal(products)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
+	log.Println("sending")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
 }
@@ -45,16 +56,20 @@ func (p *productAPI) createProduct(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&product)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	savedProduct, err := p.pa.SaveProduct(product)
 	if err != nil {
+		fmt.Println(err)
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	response, err := json.Marshal(savedProduct)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -68,11 +83,13 @@ func (p *productAPI) getProductByID(w http.ResponseWriter, r *http.Request) {
 	product, err := p.pa.GetProduct(productId)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	response, err := json.Marshal(product)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -87,11 +104,13 @@ func (p *productAPI) updateProduct(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&product)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	message, err := p.pa.UpdateProduct(product)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -105,6 +124,7 @@ func (p *productAPI) deleteProduct(w http.ResponseWriter, r *http.Request) {
 	message, err := p.pa.DeleteProduct(productId)
 	if err != nil {
 		w.Write([]byte("Error Occured"))
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
--- server/main.go
+++ server/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Prashant-sharma3012/ecom/tree/main/server/db"
+	"github.com/Prashant-sharma3012/ecom/tree/main/server/interfaces"
+	"github.com/gorilla/mux"
+)
+
 func main() {
 
+	db := db.GetDBClient()
+
+	log.Println("Setting up App")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	}).Methods("GET")
+
+	interfaces.SetupProductRoutes(r, db.Product)
+
+	s := &http.Server{
+		Addr:           ":4000",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Println("Listening on port 4000")
+	log.Fatal(s.ListenAndServe())
+
+	log.Println("hey")
 }
